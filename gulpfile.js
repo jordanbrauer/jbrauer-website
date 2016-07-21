92f491e4d2df1a471ae726c3a6da00c280831eb1
jordanbrauer <j.brauer@hotmail.com>
2016-07-21 16:15:31 -0500

    added compass config, added node, sass, and git ignores to the .gitignore file. installed gulp, gulp-compass, and foundation-sites ~6.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+gulp.task('get:foundation', function() {
+  // fetch foundation assets from node_modules
+});
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
