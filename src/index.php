170267afaa1f7627474f8c0acc36625c5774f943
jordanbrauer <jbrauer.inc@gmail.com>
2016-08-14 19:14:40 -0500

    Basic homepage layout w/ filler content.

--- src/index.php
+++ src/index.php
@@ -19,12 +19,10 @@
       <div class="top-bar" style="padding:1rem;">
         <div class="top-bar-title">
           <a href="index.php">
-            <img src="img/logo/jbrauer/png/logo-banner-jbrauer.png" alt="jBrauer Web Design &amp; Development" class="logo-topbar-md">
-            <br>
-            <small class="subheader ff-consolas">Web Design &amp; Development</small>
+            <img data-interchange="[img/logo/jbrauer/png/logo-avatar-jbrauer-cropped.png, small], [img/logo/jbrauer/png/logo-banner-jbrauer-cropped.png, large]" alt="jBrauer Web Design &amp; Development" class="logo-topbar-md">
+            <!-- <br>
+            <small class="subheader ff-consolas">Web Design &amp; Development</small> -->
           </a>
-
-
 
           <!-- | Small logo (medium screen +) | -->
           <div class="top-bar-logo text-center hide-for-small-only hide" style="position:fixed; top:0; z-index:10; margin:0.75rem;">
@@ -36,7 +34,7 @@
           </div> <!-- /#top-bar-logo -->
 
           <!-- | Medium hire me button | -->
-          <a class="small success button" href="#" style="position: absolute; top:0; right:0; z-index:10; margin: 0.75rem;">Hire Me!</a>
+          <a class="small hollow button" href="#" style="position: absolute; top:0; right:0; z-index:10; margin: 0.75rem;">Affiliates</a>
           <a id="btn-hire" class="small success button hide" href="#" style="position: fixed; top:0; right:0; z-index:10; margin: 0.75rem;">Hire Me!</a>
 
           <div class="clearfix"></div>
@@ -44,12 +42,19 @@
       </div> <!-- /#top-bar-1 -->
 
       <!-- | Hero Header | -->
-      <header id="header-hero" class="header-hero">
+      <header id="header-hero" class="header-hero text-center">
+        <h1>Changing the World Through Design</h1>
+        <p class="lead subheader">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <br>
+        <a class="large button" href="#">Learn More</a>
+        <a class="large success button" href="#">Hire Me!</a>
       </header>
 
       <!-- | Sticky Navigation | -->
       <nav data-sticky-container>
-        <div id="sticky-navigation" class="sticky" data-sticky data-margin-top="0" data-top-anchor="header-hero:bottom" data-btm-anchor="section-main:bottom" data-sticky-on="small">
+        <div id="sticky-navigation" class="sticky" data-sticky data-margin-top="0" data-top-anchor="header-hero:bottom" data-btm-anchor="section-footer:bottom" data-sticky-on="small">
 
           <!-- | Responsive Menu Toggle | -->
           <div id="responsive-menu-toggle" class="top-bar hide-for-medium">
@@ -90,7 +95,7 @@
                   <li class="active"><a href="#">About</a></li>
                   <li><a href="#">Services</a></li>
                   <li><a href="#">Contact</a></li>
-                  <li><a href="#">Resume</a></li>
+                  <li><a href="#">Portfolio</a></li>
                 </ul>
               </div> <!-- /.top-bar-left -->
               <div class="top-bar-right">
@@ -115,53 +120,206 @@
 
     <!-- | Main Content Section | -->
     <div id="section-main">
-      <div class="row column">
-        <h3>Creating a sticky Navigation Menu with Top Bar!</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate est in inventore fugit repudiandae aliquid maxime minus vitae quae quidem? Non saepe harum eaque voluptatem eveniet sapiente, explicabo expedita facere.</p>
-        <hr>
+      <div class="row">
+        <div class="columns small-12 medium-6 large-6">
+          <h2>About Me &amp; What I Do</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate est in inventore fugit repudiandae aliquid maxime minus vitae quae quidem? Non saepe harum eaque voluptatem eveniet sapiente, explicabo expedita facere.</p>
+          <a class="small hollow button" href="#">Learn More</a>
+        </div>
+        <div class="columns small-12 medium-6 large-6">
+          <img class="thumbnail" src="http://placehold.it/750x350?text=content" alt="placeholder">
+        </div>
+        <!-- <hr> -->
       </div><!-- /.row -->
 
       <div class="row">
+        <div class="column">
+          <hr>
+        </div> <!-- /.column -->
         <div class="columns small-12 medium-4 large-4">
           <div class="path-item">
             <div class="circle">
-              <h3>1</h3>
+              <h3>Design</h3>
             </div> <!-- /.circle -->
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
           </div> <!-- /.path-item -->
         </div> <!-- /.column -->
         <div class="columns small-12 medium-4 large-4">
           <div class="path-item">
             <div class="circle">
-              <h3>2</h3>
+              <h3>Development</h3>
             </div> <!-- /.circle -->
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
           </div> <!-- /.path-item -->
         </div> <!-- /.column -->
         <div class="columns small-12 medium-4 large-4">
           <div class="path-item">
             <div class="circle">
-              <h3>3</h3>
+              <h3>SEO</h3>
             </div> <!-- /.circle -->
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
           </div> <!-- /.path-item -->
+        </div> <!-- /.column -->
+        <div class="column">
+          <hr>
         </div> <!-- /.column -->
       </div> <!-- /.row -->
 
-      <div class="row column buffer-m-t-64">
-        <h3>Creating a sticky Navigation Menu with Top Bar!</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate est in inventore fugit repudiandae aliquid maxime minus vitae quae quidem? Non saepe harum eaque voluptatem eveniet sapiente, explicabo expedita facere.</p>
-        <hr>
-        <img src="http://placehold.it/1200x1200&text=content" alt="placeholder1" class="thumbnail">
-        <hr>
-        <img src="http://placehold.it/1200x1200&text=content" alt="placeholder2" class="thumbnail">
-        <hr>
-        <img src="http://placehold.it/1200x1200&text=content" alt="placeholder3" class="thumbnail">
+      <div class="row column">
+        <h3>Recent Work</h3>
+      </div>
+
+      <div class="row small-up-1 medium-up-2 large-up-3">
+        <div class="column">
+          <div class="media-object">
+            <div class="media-object-section">
+              <img src="http://placehold.it/175x175&text=content" alt="placeholder1" class="thumbnail">
+            </div> <!-- /.media-object-section -->
+            <div class="media-object-section">
+              <h4>Project Title</h4>
+              <p class="subheader">Small description of the project or your role in the project.</p>
+            </div>
+          </div> <!-- /.media-object -->
+        </div> <!-- /.column -->
+        <div class="column">
+          <div class="media-object">
+            <div class="media-object-section">
+              <img src="http://placehold.it/175x175&text=content" alt="placeholder1" class="thumbnail">
+            </div> <!-- /.media-object-section -->
+            <div class="media-object-section">
+              <h4>Project Title</h4>
+              <p class="subheader">Small description of the project or your role in the project.</p>
+            </div>
+          </div> <!-- /.media-object -->
+        </div> <!-- /.column -->
+        <div class="column">
+          <div class="media-object">
+            <div class="media-object-section">
+              <img src="http://placehold.it/175x175&text=content" alt="placeholder1" class="thumbnail">
+            </div> <!-- /.media-object-section -->
+            <div class="media-object-section">
+              <h4>Project Title</h4>
+              <p class="subheader">Small description of the project or your role in the project.</p>
+            </div>
+          </div> <!-- /.media-object -->
+        </div> <!-- /.column -->
+        <div class="column">
+          <div class="media-object">
+            <div class="media-object-section">
+              <img src="http://placehold.it/175x175&text=content" alt="placeholder1" class="thumbnail">
+            </div> <!-- /.media-object-section -->
+            <div class="media-object-section">
+              <h4>Project Title</h4>
+              <p class="subheader">Small description of the project or your role in the project.</p>
+            </div>
+          </div> <!-- /.media-object -->
+        </div> <!-- /.column -->
       </div> <!-- /.row -->
     </div> <!-- /#section-main -->
 
+    <div class="row column">
+      <hr>
+      <ul class="vertical medium-horizontal menu expanded text-center">
+        <li>
+          <a href="#">
+            <div class="stat">0</div>
+            <span>Websites</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="stat">0</div>
+            <span>Apps</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="stat">0</div>
+            <span>Ads</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="stat">0</div>
+            <span>Logos</span>
+          </a>
+        </li>
+      </ul>
+      <hr>
+    </div> <!-- /.row -->
+
     <!-- | Footer Section | -->
-    <div id="section-footer" class="row expanded">
+    <div id="section-footer">
+      <footer class="footer" style="padding: 2rem 0; background: #e6e6e6;">
+        <div class="row">
+          <div class="columns small-12 medium-6 large-6">
+            <h6>Contact Me</h6>
+            <form action="#" method="post">
+              <div class="row">
+                <div class="columns small-12 medium-6 large-6">
+                  <label for="firstname">* First Name:
+                    <input id="firstname" name="firstnamename" type="text" placeholder="First Name" required>
+                  </label>
+                </div> <!-- /.column -->
+                <div class="columns small-12 medium-6 large-6">
+                  <label for="lastname">Last Name:
+                    <input id="lastname" name="lastname" type="text" placeholder="Last Name">
+                  </label>
+                </div> <!-- /.column -->
+              </div> <!-- /.row -->
+              <div class="row">
+                <div class="columns small-12 medium-12 large-12">
+                  <label for="email">* Email:
+                    <input id="email" name="email" type="email" placeholder="someone@example.com" required>
+                  </label>
+                </div> <!-- /.column -->
+              </div> <!-- /.row -->
+              <div class="row">
+                <div class="column small-12 medium-12 large-12">
+                  <label for="message">* Message:
+                    <textarea id="message" name="message" placeholder="Message..." required></textarea>
+                  </label>
+                </div> <!-- /.column -->
+              </div> <!-- /.row -->
+              <div class="row">
+                <div class="column small-12 medium-12 large-12">
+                  <input id="submit" class="expanded button" name="submit" type="submit" value="Send">
+                </div> <!-- /.column -->
+              </div> <!-- /.row -->
+            </form>
+          </div> <!-- /.column -->
+          <div class="columns small-12 medium-3 large-3">
+            <h6>Affiliates</h6>
+            <ul class="vertical medium-vertical menu">
+              <li><a href="#">DigitalOcean</a></li>
+              <li><a href="#">Freshbooks</a></li>
+              <li><a href="#">Namecheap</a></li>
+            </ul>
+          </div> <!-- /.column -->
+          <div class="columns small-12 medium-3 large-3">
+            <h6>Follow Me</h6>
+            <ul class="vertical medium-vertical menu">
+              <li><a href="#"><i class="fa fa-lg fa-facebook"></i> /jbrauerwebdesign</a></li>
+              <li><a href="#"><i class="fa fa-lg fa-twitter"></i> @jordbrauer</a></li>
+              <li><a href="#"><i class="fa fa-lg fa-github"></i> @jordanbrauer</a></li>
+              <li><a href="#"><i class="fa fa-lg fa-codepen"></i> @jordanbrauer</a></li>
+              <li><a href="#"><i class="fa fa-lg fa-linkedin"></i> /jordanbrauer</a></li>
+            </ul>
+          </div> <!-- /.column -->
+        </div> <!-- /.row -->
+        <div class="row column buffer-m-t-32" style="font-size: 12px">
+          <ul class="horizontal menu pull-left">
+            <li class="subheader">jBrauer Web Design &amp; Development &copy; 2016</li>
+          </ul>
+          <ul class="horizontal menu pull-right">
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms of Service</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+          <span class="clearfix"></span>
+        </div> <!-- /.row -->
+      </footer>
     </div> <!-- /#section-footer -->
 
     <!-- | External Scripts | -->
