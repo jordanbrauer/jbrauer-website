eed70c7f407c06eafe859c4a57734ecb465bc1f8
jordanbrauer <jbrauer.inc@gmail.com>
2017-01-25 19:04:51 -0600

    Added boilerplate gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+/**
+ * gulpfile.js
+ * @author Jordan Brauer <jbrauer.inc@gmail.com>
+ */
+
+/** Load gulp, package (project) data, and plugin loader. */
+const gulp = require('gulp');
+const pkg = require('./package.json');
+const plugins = require('gulp-load-plugins')({
+  pattern: [
+    'gulp-*',
+    'gulp.*',
+    'del',
+    'merge-stream',
+  ],
+});
+
+/**
+ * @function uses({string} task)
+ * @description Helper function for loading a tasks' script.
+ */
+const uses = (task) => {
+  return require('./dev/tasks/' + task)(gulp, plg, plugins);
+}
